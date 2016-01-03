9c323e47f3f49fa57dd10202fee3c53c60b38580
Márton Boros <martonboros@gmail.com>
2016-01-03 20:20:13 +0000

    util doc

--- src/Lifty/Util.elm
+++ src/Lifty/Util.elm
@@ -18,32 +18,44 @@ s_ = toString
 
 f_ = toFloat
 
+-- Return a list containing the integers from zero to n-1
 zeroTo : Int -> List Int
 zeroTo n = L.repeat n () |> L.indexedMap (\a b -> a)
 
+-- Same as `zipWithIndex`
 izipL : List a -> List (Int, a)
 izipL l = A.toIndexedList (A.fromList l)
 
+-- Same as `zipWithIndex`, start with an Array
 izipA : Array a -> List (Int, a)
 izipA l = A.toIndexedList l
 
+-- Same as `flip mapWithIndex`
 imapL : List a -> ((Int, a) -> b) -> List b
 imapL l f = L.indexedMap (curry f) l
 
+-- Same as `flip mapWithIndex`, start with an array
 imapA : Array a -> ((Int, a) -> b) -> List b
 imapA l f = L.map f (izipA l)
 
+-- Shorthand for defining an animation with common parameters
 anim start from to duration =
   Ani.animation start |> Ani.from from |> Ani.to to |> Ani.duration duration
 
+-- Return the effect resulting from delaying an action
 delay : Time -> a -> Effects a
 delay t action = E.task <| Task.Extra.delay t <| Task.succeed <| action
 
+-- Return an effect resulting from transforming and delaying an action, or none
 schedule : (a -> b) -> Maybe (Time, a) -> Effects b
 schedule act ma = M.map (\(dt, a) -> delay dt (act a)) ma ? E.none
 
-mkM : Address a -> (b -> a) -> b -> Message
+-- Given an `Address a` and a function that returns `a`,
+-- return a function that returns a `Message`
+mkM : Address a -> (b -> a) -> (b -> Message)
 mkM address action param = S.message address (action param)
 
-mkM2 : Address a -> (c -> b -> a) -> c -> b -> Message
+-- Given an `Address a` and a two-parameter function that returns `a`,
+-- return a two-parameter function that returns a `Message`
+mkM2 : Address a -> (c -> b -> a) -> (c -> b -> Message)
 mkM2 address action param1 param2 = S.message address (action param1 param2)
